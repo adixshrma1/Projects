366207bba03b4d705da910bee615e240ded517c7
adixshrma <aditya.sharma.26092001@gmail.com>
2024-04-13 11:41:40 +0530

    7th modified, 8th added

--- javascript-mini/07quoteGenerator/script.js
+++ javascript-mini/07quoteGenerator/script.js
@@ -2,14 +2,14 @@ let quotes = document.getElementById('quotes');
 let writer = document.getElementById('writer');
 let url = 'https://api.quotable.io/random';
 
-async function getQuote(url){
+async function getQuote(){
     const response = await fetch(url);
     const data = await response.json();
 
     quotes.innerHTML = data.content;
     writer.innerHTML = data.author;
 }
-getQuote(url);
+getQuote();
 
 function tweetIt(){
     window.open("https://twitter.com/intent/tweet?text=" + quotes.innerHTML + "---by " + writer.innerHTML, "window", "height=400, width=700")
